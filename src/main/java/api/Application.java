a573c879a40341ce5c9ad831f86795867cc0e5bf
Olavi Sau <olavisau@gmail.com>
2018-11-11 19:13:22 +0200

    Allow another path

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -19,6 +19,7 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/categories").allowedOrigins("*");
+                registry.addMapping("/categories/*").allowedOrigins("*");
             }
         };
     }
