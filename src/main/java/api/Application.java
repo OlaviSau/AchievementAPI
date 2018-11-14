b9197150619926a76b0f43352e4b12c7ec931f2c
Olavi Sau <olavisau@gmail.com>
2018-11-14 06:09:09 +0200

    More cors

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -18,8 +18,8 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/category").allowedOrigins("*");
-                registry.addMapping("/category/*").allowedOrigins("*").allowedMethods("GET", "PUT");
+                registry.addMapping("/category").allowedOrigins("*").allowedMethods("GET", "PUT");
+                registry.addMapping("/category/*").allowedOrigins("*");
             }
         };
     }
